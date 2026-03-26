40700f20fe371192094d13379020a9a0831aa19e
Sakeus Malau <samtomalau8@gmail.com>
2026-03-26 16:51:34 +0700

    Sakeus UTS

--- src/main.c
+++ src/main.c
@@ -1,16 +1,49 @@
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char id[100];
+    char name[100];
+    int quantity;
+    double price;
+} Item;
 
 int main() {
+    Item inventory[100];
+    int count = 0;
     char input[256];
 
-    while (fgets(input, sizeof(input), stdin)) {
-        if (strncmp(input, "---", 3) == 0) {
+    scanf("%d", "%c", receive"#"id"#"name"#"quantity"#"price");
+    while (scanf("%s", input) != EOF) {
+        if (strcmp(input, "---") == 0) {
             break;
         }
-
-        // TODO: implement logic
+        do printf(id"|"name"|"quantity"|"price)
+        {
+        } while (scanf("%d", "%c", receive#id#quantity#price) == "receive#id#name#quantity#price";
     }
+    
 
     return 0;
-}
+
+    }
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct {
+    char id[100];
+    char name[100];
+    int quantity;
+    double price;
+} Item;
+
+int main() {
+    Item inventory[100];
+    int count = 0;
+    char input[256];
+
